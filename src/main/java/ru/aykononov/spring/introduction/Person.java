b3e5f94cbb469b7ce6487eca9c206f65a1f3306a
aykononov <aykononov@yandex.ru>
2023-09-26 21:27:00 +0300

    урок 8 Dependency Injection с помощью сеттера

--- src/main/java/ru/aykononov/spring/introduction/Person.java
+++ src/main/java/ru/aykononov/spring/introduction/Person.java
@@ -3,8 +3,17 @@ package ru.aykononov.spring.introduction;
 public class Person {
     private Pet pet;
 
+    public Person() {
+        System.out.println("Person bean is created");
+    }
+
     public Person(Pet pet) {
         System.out.println("Person bean is created");
+        this.pet = pet;
+    }
+
+    public void setPet(Pet pet) {
+        System.out.println("Person: set Pet");
         this.pet = pet;
     }
 
